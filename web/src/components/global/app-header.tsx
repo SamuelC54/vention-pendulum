9b825e6311edae847fb426e4d7e5dd11c4716e6e
Samuel Croteau <SCroteau94@gmail.com>
2024-09-27 15:53:43 -0400

    Add signature

--- web/src/components/global/app-header.tsx
+++ web/src/components/global/app-header.tsx
@@ -7,6 +7,12 @@ export function AppHeader() {
     <div className={'flex items-center border-b px-8 py-2'}>
       <Logo />
       <span className="flex-1" />
+      <div className="text-sm italic text-gray-500">
+        Made by{' '}
+        <a href="https://samuelcroteau.com" className="text-blue-500 underline">
+          Samuel Croteau
+        </a>
+      </div>{' '}
     </div>
   );
 }
